a5b4dae697531363b782f42b916750e35b168019
kiruthiga1793 <pkiruthiga93@gmail.com>
2025-02-23 11:51:38 -0500

    RATIS-2224

--- dev-support/checks/examples.sh
+++ dev-support/checks/examples.sh
@@ -26,7 +26,10 @@ cd  ratis-examples/src/main/bin
 
 echo $EXAMPLES_DIR/ratis-examples/src/main/bin
 
+
 ARITH_SERVER_DIR=$EXAMPLES_DIR/ratis-examples/src/main/bin
+
+source $ARITH_SERVER_DIR/server.sh
 
 source $ARITH_SERVER_DIR/start-all.sh arithmetic server
 
