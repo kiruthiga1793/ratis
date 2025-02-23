6cf9004fb4ffe02c46fab0ebcc66a420bf8236e3
kiruthiga1793 <pkiruthiga93@gmail.com>
2025-02-23 15:38:21 -0500

    RATIS-2224

--- ratis-examples/src/main/bin/common.sh
+++ ratis-examples/src/main/bin/common.sh
@@ -38,7 +38,7 @@ else
    JAR_PREFIX=`basename ${EXAMPLES_DIR}`
    echo "JAR_PREFIX inside common.sh"  $JAR_PREFIX
    echo "list target folder in examples dir"
-   ls -d ${EXAMPLES_DIR}/
+   ls -d ${EXAMPLES_DIR}/target/
    ARTIFACT=`ls -1 ${EXAMPLES_DIR}/target/${JAR_PREFIX}-*.jar | grep -v test | grep -v javadoc | grep -v sources | grep -v shaded`
    echo "Printing Artifacts inside common.sh"  $ARTIFACT
    if [[ ! -f "$ARTIFACT" ]]; then
