a167581967881b5343531769efa1f1d580fc1e54
kiruthiga1793 <pkiruthiga93@gmail.com>
2025-03-26 17:47:57 -0400

    added examples.sh  file

--- dev-support/checks/examples.sh
+++ dev-support/checks/examples.sh
@@ -22,10 +22,7 @@ EXAMPLES_DIR=${DIR}/../../
 
 echo "EXAMPLES_DIR"   $EXAMPLES_DIR
 
-echo "Starting Maven clean install..."
 
-# Run the Maven clean and install command
-mvn clean install -DskipTests
 
 cd  ratis-examples/src/main/bin
 
